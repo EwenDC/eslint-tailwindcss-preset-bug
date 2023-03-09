f6a4b0e5798d79589fab6d72a2aac7b81a6a2894
Ewen Cameron <ewen.cameron@megaport.com>
2023-03-09 17:35:13 +1000

    Fixed incorrect preset import

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [
-    require('./tailwind-preset'),
+    require('@ewendc/tailwind-preset'),
   ],
   // theme: {
   //   colors: {
